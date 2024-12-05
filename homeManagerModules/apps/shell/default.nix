1fce69337a58c1d3270f9912186698d9b0f79fa3
Aly Raffauf <aly@raffauflabs.com>
2024-12-05 15:31:43 -0500

    hm/shell: drop bash

--- homeManagerModules/apps/shell/default.nix
+++ homeManagerModules/apps/shell/default.nix
@@ -11,28 +11,6 @@
     };
 
     programs = {
-      bash = {
-        enable = true;
-        enableCompletion = true;
-        enableVteIntegration = true;
-
-        shellOptions = [
-          "autocd"
-          "cdspell"
-          "checkjobs"
-          "checkwinsize"
-          "dirspell"
-          "dotglob"
-          "extglob"
-          "globstar"
-          "histappend"
-        ];
-
-        initExtra = ''
-          export PS1="[\[$(tput setaf 27)\]\u\[$(tput setaf 135)\]@\[$(tput setaf 45)\]\h:\[$(tput setaf 33)\]\w] \[$(tput sgr0)\]$ "
-        '';
-      };
-
       direnv.enable = true;
 
       eza = {
