fce6fa47aeec2b05bf2cf0183f3401c95fda0461
Aly Raffauf <aly@raffauflabs.com>
2024-12-20 22:01:33 -0500

    overlays: remove electron wayland IME overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,22 +3,4 @@
   adjustor = self.packages.${prev.system}.adjustor;
   emudeck = self.packages.${prev.system}.emudeck;
   steam-rom-manager = self.packages.${prev.system}.steam-rom-manager;
-
-  brave = prev.brave.override {commandLineArgs = "--gtk-version=4 --enable-wayland-ime";};
-
-  obsidian = prev.obsidian.overrideAttrs (old: {
-    installPhase =
-      builtins.replaceStrings ["--ozone-platform=wayland"]
-      ["--ozone-platform=wayland --enable-wayland-ime"]
-      old.installPhase;
-  });
-
-  vscodium = prev.vscodium.override {commandLineArgs = "--enable-wayland-ime";};
-
-  webcord = prev.webcord.overrideAttrs (old: {
-    installPhase =
-      builtins.replaceStrings ["--ozone-platform-hint=auto"]
-      ["--ozone-platform-hint=auto --enable-wayland-ime"]
-      old.installPhase;
-  });
 }
